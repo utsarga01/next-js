cf20dfb40a9ead2017271e819bb2b09c83dc446f
utsarga01 <regmiutsarga7@gmail.com>
2025-01-02 14:48:31 +0545

    sellerlist

--- components/BuyerList.jsx
+++ components/BuyerList.jsx
@@ -1,6 +1,8 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 
 const BuyerList = () => {
+  const {data,isPending,error}= useQuery
   return (
     <div>BuyerList</div>
   )
--- components/SellerList.jsx
+++ components/SellerList.jsx
@@ -1,22 +1,55 @@
-import $axios from '@/lib/axios.instance'; 
-import React, { useEffect } from 'react'; 
- 
-const SellerList = () => { 
-  useEffect(() => { 
-    const getProduct = async () => { 
-      try { 
-        const res = await $axios.post('/product/seller/list', { 
-          page: 1, 
-          limit: 10, 
-        }); 
-        console.log(res); 
-      } catch (error) { 
-        console.log('error vayo'); 
-      } 
-    }; 
-    getProduct(); 
-  }, []); 
-  return <div>Seller List</div>; 
-}; 
- 
+import $axios from "@/lib/axios.instance";
+import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
+import { CircularProgress, Pagination } from "@mui/material";
+
+const SellerList = () => {
+  const [productList, setProductList] = useState([]);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState("");
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    const getSellerProduct = async () => {
+      try {
+        setIsPending(true);
+        const res = await $axios.post("/product/seller/list", {
+          page: page,
+          limit: 2,
+        });
+        setIsPending(false);
+        setProductList(res?.data?.productList);
+      } catch (error) {
+        setError("Something  went wrong.");
+        setIsPending(false);
+      }
+    };
+    getSellerProduct();
+  }, [page]);
+  if (isPending) {
+    return <CircularProgress />;
+  }
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+  return (
+    <div className="flex flex-col justify-between items-center gap-8">
+      <div>
+        {productList.map((item) => {
+          return <ProductCard key={item._id} {...item} />;
+        })}
+      </div>
+      <Pagination
+        page={page}
+        count={5}
+        color="secondary"
+        className="my-12"
+        size="large"
+        onChange={(_, value) => {
+          setPage(value);
+        }}
+      />
+    </div>
+  );
+};
+
 export default SellerList;
